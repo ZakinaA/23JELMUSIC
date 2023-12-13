1041af54943b192fe8010418d599871431b5a8c7
llouna <louna.constantinescu04@gmal.com>
2023-12-13 09:54:36 +0100

    ajout de la page d'acceuil

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContratPret;
+use App\Entity\Cours;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AcceuilController extends AbstractController
+{
+
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Cours::class);
+        $cours = $repository->findAll();
+
+        return $this->render('acceuil/index.html.twig', [
+            'controller_name' => 'AcceuilController',
+            'pCours' => $cours,
+        ]);
+    }
+}
