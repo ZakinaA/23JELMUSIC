59c73ca378c57ae67e5761d23fe856acc670b186
Jules <jules.turpin76620@gmail.com>
2023-12-13 08:24:10 +0100

    création de l'entity User

---
+++ migrations/Version20231213072342.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231213072342 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE instrument CHANGE num_serie num_serie VARCHAR(30) NOT NULL, CHANGE utilisation utilisation VARCHAR(20) DEFAULT NULL, CHANGE nom nom VARCHAR(30) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE instrument CHANGE num_serie num_serie VARCHAR(255) NOT NULL, CHANGE utilisation utilisation VARCHAR(100) DEFAULT NULL, CHANGE nom nom VARCHAR(70) NOT NULL');
+    }
+}
